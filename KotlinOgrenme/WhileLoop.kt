f3a3c9895cc2cf3207bfebcdc3b1d0fef0780f8b
mrdyasar1 <59768661+mrdyasar1@users.noreply.github.com>
2020-03-17 21:39:52 +0300

    Create WhileLoop.kt

---
+++ KotlinOgrenme/WhileLoop.kt
@@ -0,0 +1,21 @@
+package KotlinOgren
+
+fun main() {
+    //While Loop
+
+    //while da bir döngü çeşididir. diğerlerinden farkı sonu belli değildir
+    //sonsuz bi döngü oluşturabiliriz
+
+    var j = 0
+    while (j < 10){
+        println(j)
+        j = j + 1
+        //bunu yapmamızın nedeni eğer 1 olarak arttırmazsak
+        //sonsuz döngüye girer ve bilgisayarı kitler
+        //ayrıca j = j + 1 yerine j++ kullanabilirdik
+
+        //while da koşulu olması lazım
+    }
+
+
+}
