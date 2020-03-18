eefce14f7cc2fdc0c80eb1f59c24aae6b5420f33
mrdyasar1 <59768661+mrdyasar1@users.noreply.github.com>
2020-03-18 15:55:43 +0300

    Create FuntcionLearn.kt

---
+++ KotlinOgrenme/FuntcionLearn.kt
@@ -0,0 +1,18 @@
+package KotlinOgren
+
+    fun main() {
+    /* fonksiyonlar büyük programları parça parça ayırmamızı sağlayarak
+    daha düzenli kodlar yazmamızı sağlar
+    ayrıca kodları içinde sakladıkları için başka bir yerde kolayca tüm kodları çağırabiliriz
+    fonksiyonlar fun ile oluşturulur
+
+     */
+        test()
+}
+
+fun test(){
+    // test() adında yeni bir fonskiyon oluşturduk
+    // {} arasında yazdıklarımız test fonskiyonun altında çalıştırılır
+    //oluşturduğumuz her fonskiyonu kessinlikle çağrımamız gerekir
+    println("Hello Test")
+}
